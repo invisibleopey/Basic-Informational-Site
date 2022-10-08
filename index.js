32864f2b5ec8b9cde647da386f9d71e7d7f97244
Abdullahi Muritala <oabdullahi37@gmail.com>
2022-10-08 03:45:20 +0100

    Clean up code, remove pseudocode

--- index.js
+++ index.js
@@ -1,18 +1,3 @@
-// Require Path
-// Require fs
-// Require http
-
-// create an http server
-// Listen to the http server at a particular port
-
-// On request, capure the url of the server
-// Initiate a targetPage to index.html
-// If others, use switch to set them to the appropriate file
-// Now, join the paths with the resulatant variable to look for a file
-// Use the fs readfile to read the file
-// Use the contnt to res.(end)
-// Set the status codes within the switch bodies
-
 const path = require('path');
 const fs = require('fs');
 const http = require('http');
