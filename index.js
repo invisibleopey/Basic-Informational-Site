29196dd01a58f841a59055f85917dc52e71c5bef
Abdullahi Muritala <oabdullahi37@gmail.com>
2022-10-08 03:41:18 +0100

    Create a server and route the response properly

--- index.js
+++ index.js
@@ -12,3 +12,40 @@
 // Use the fs readfile to read the file
 // Use the contnt to res.(end)
 // Set the status codes within the switch bodies
+
+const path = require('path');
+const fs = require('fs');
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  // Initiate the target file to error page by default, correct that per req in switch statement
+  let targetFile = '404.html';
+  res.setHeader('Content-Type', 'text/html');
+
+  switch (req.url) {
+    case '/':
+      targetFile = 'index.html';
+      break;
+
+    case '/about':
+      targetFile = 'about.html';
+      break;
+
+    case '/contact-me':
+      targetFile = 'contact-me.html';
+      break;
+
+    default:
+      break;
+  }
+
+  fs.readFile(path.join(__dirname, targetFile), (err, data) => {
+    if (err) throw err;
+    res.statusCode = 200;
+    res.end(data);
+  });
+});
+
+server.listen(8000, () => {
+  console.log(`Server running at: http://127.0.0.1:8000`);
+});
