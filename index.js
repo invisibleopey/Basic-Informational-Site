059399cffbde6073963656426673c887e8308180
Abdullahi Muritala <oabdullahi37@gmail.com>
2022-10-12 15:30:13 +0100

    Add routes to other pages and not found page

--- index.js
+++ index.js
@@ -10,6 +10,30 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
+app.get('/', (req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/about', (req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  res.sendFile(path.join(__dirname, 'about.html'));
+});
+
+app.get('/contact-me', (req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  res.sendFile(path.join(__dirname, 'contact-me.html'));
+});
+
+app.use((req, res) => {
+  res.statusCode = 404;
+  res.setHeader('Content-Type', 'text/html');
+  res.sendFile(path.join(__dirname, '404.html'));
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`);
 });
