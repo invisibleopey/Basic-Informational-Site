8a339d72b1c2b0c60ecf1848cd47312e308bf3ee
Abdullahi Muritala <oabdullahi37@gmail.com>
2022-10-12 15:28:34 +0100

    Set up routes for the homepage and listen at 3000

--- index.js
+++ index.js
@@ -1,36 +1,15 @@
 const path = require('path');
-const fs = require('fs');
-const http = require('http');
+const express = require('express');
+const app = express();
 
-const server = http.createServer((req, res) => {
-  // Initiate the target file to error page by default, correct that per req in switch statement
-  let targetFile = '404.html';
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
-
-  switch (req.url) {
-    case '/':
-      targetFile = 'index.html';
-      break;
-
-    case '/about':
-      targetFile = 'about.html';
-      break;
-
-    case '/contact-me':
-      targetFile = 'contact-me.html';
-      break;
-
-    default:
-      break;
-  }
-
-  fs.readFile(path.join(__dirname, targetFile), (err, data) => {
-    if (err) throw err;
-    res.statusCode = 200;
-    res.end(data);
-  });
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-server.listen(8000, () => {
-  console.log(`Server running at: http://127.0.0.1:8000`);
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}!`);
 });
